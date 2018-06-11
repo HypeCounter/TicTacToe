c776b45ee189c48cd4c2de2635cd763e2b4edfa4
FallenPaladin <oliveira.chinape@gmail.com>
2018-06-11 12:28:50 -0300

    Update Camera
    dd

---
+++ Assets/Scripts/AjustaCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AjustaCamera : MonoBehaviour {
+    private float orthSize = 5;
+    [SerializeField] private float aspect = 1.66f;
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(-orthSize * aspect, orthSize * aspect, -orthSize, orthSize, Camera.main.nearClipPlane, Camera.main.farClipPlane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
